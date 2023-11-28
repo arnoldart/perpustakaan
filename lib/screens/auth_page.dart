d8c4c17509c96f856f5abaed476438ba2322f5ec
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2023-11-28 14:03:57 +0700

    first commit

---
+++ lib/screens/auth_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Username'),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.pink)
+                    ),
+                    fillColor: Colors.grey
+                  ),
+                ),
+              ),
+              Text('asdasd'),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
